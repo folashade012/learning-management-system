2623a15a01638c47ea9af2e69dc8f78b78c9006d
Onikoko Abdullahi <onikokoabdullahi@gmail.com>
2023-12-10 10:54:36 +0100

    Updated Images

--- app/components/slider-main.tsx
+++ app/components/slider-main.tsx
@@ -24,7 +24,7 @@ export default function SliderMain({ images }: CarouselProps) {
   };
 
   return (
-    <div className='relative'>
+    <div className='relative w-full'>
       <div>
         <button
           onClick={prevImage}
